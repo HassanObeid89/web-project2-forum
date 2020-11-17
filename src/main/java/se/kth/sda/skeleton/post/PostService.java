4bcdc04c7dd113444f9aa8fe0813d55b8d03e4e3
richagupta <gptaricha@gmail.com>
2020-11-17 14:00:23 +0100

    Added Post entity, controller and repo

---
+++ src/main/java/se/kth/sda/skeleton/post/PostService.java
@@ -0,0 +1,34 @@
+package se.kth.sda.skeleton.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getALL() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> getById(Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    public Post create(Post newPost) {
+        return postRepository.save(newPost);
+    }
+
+    public Post update(Post updatedPost) {
+        return postRepository.save(updatedPost);
+    }
+
+    public void delete(Long postId) {
+        postRepository.deleteById(postId);
+    }
+
+}
