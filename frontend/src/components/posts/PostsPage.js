1347be000362205b93524a4b1be88e53bef0e97a
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2020-11-18 11:32:10 +0100

    added two features to the post component create and post

--- frontend/src/components/posts/PostsPage.js
+++ frontend/src/components/posts/PostsPage.js
@@ -1,27 +1,33 @@
 import React, {useState, useEffect} from "react";
 import PostsApi from "../../api/PostsApi";
 import PostForm from "./PostForm";
-import Api from "../../api/Api"
+import Api from "../../api/Api";
+import PostsList from "./PostsList";
 
 function PostsPage() {
 
-    // const [body, setBody] = useState(null);
+    const [posts, setPosts] = useState([]);
 
-    // useEffect(() => {
-    //     Api.get("/posts")
-    //     .then  (r => {
-    //         const body = r.data;
-    //         setBody(body)
-    //     });
-    // }, []);
+    useEffect(() => {
+        Api.get("/posts")
+            .then((res) => setPosts(res.data))
+    },[]);
+    
+
+    const createPost = (postData) => {
+        Api.post("/posts", postData)
+            .then((r) => console.log(r));
+
+    }
 
     
   
 
     return (
-        <div>
-            {/* <div>here is the comments{body}</div> */}
-            {/* <PostForm onSubmit={comment}/> */}
+        <div className="card">
+            <PostForm onSubmit= {createPost} />
+            <PostsList data= {posts}/>
+           
         </div>
     );
 }
--- src/main/java/se/kth/sda/skeleton/post/Post.java
+++ src/main/java/se/kth/sda/skeleton/post/Post.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-
 public class Post {
 
     @Id
