4f6fb1e8f315a9e2d2124099677d0556f439728d
Saurabh Chauhan <saurabh311@gmail.com>
2020-11-20 12:07:34 +0100

    Remove manual foorter from HomePage

--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -75,30 +75,7 @@ function HomePage() {
                 <p className="p-title">Environment pollution</p>
                 </div>
             </div>
-            {/* </div>Contact & Social Media Information */}
-            <footer className="footer">
-                <ul className="contact-list">
-                
-                    <li>
-                        <a href="https://https://www.instagram.com/saveearthofficial/" target="blank"> Instagram
-                        <i className="fab fa-instagram-square"></i>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a href="https://www.facebook.com/EarthAndTheEnvironment.LR/" target="blank" > Facebook
-                        <i className="fab fa-facebook"></i>
-                        </a>
-                    </li>  
-                    
-                    <li>
-                        <a href="https://www.youtube.com/watch?v=AW4q0a2lfm8/" target="blank" > Youtube
-                        <i class="fab fa-youtube"></i>
-                        </a>
-                    </li> 
-                </ul>    
-            </footer>  
-
+            
         </div>
     );
 }
